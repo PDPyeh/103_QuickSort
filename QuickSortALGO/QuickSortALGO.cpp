ea27d070fcf52e1d9b7994725cf99d4ff8fcf0fa
Pradipa Y <merakayamsstraat@gmail.com>
2024-03-29 08:34:20 +0700

    void input

--- QuickSortALGO/QuickSortALGO.cpp
+++ QuickSortALGO/QuickSortALGO.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void input(int arr[], int size) {
+    cout << "Masukkan elemen array:\n";
+
+    for (int i = 0; i < size; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
 
